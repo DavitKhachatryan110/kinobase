22faf825c458c91d31e9b9ff9debfffde1b0c668
DavitKhachatryan110 <davitkhachatryan7@mail.ru>
2020-07-29 14:41:59 +0400

    Updates

---
+++ precache-manifest.4827aabd60aed7f510d5c8ad724a4695.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145decc5ca74ac05ddefea0abe497049",
+    "url": "/kinobase/index.html"
+  },
+  {
+    "revision": "48cb3922a2fd84a96f33",
+    "url": "/kinobase/static/css/2.adec7a29.chunk.css"
+  },
+  {
+    "revision": "0bf220c02fc2534234b3",
+    "url": "/kinobase/static/css/main.9f0ea2c6.chunk.css"
+  },
+  {
+    "revision": "48cb3922a2fd84a96f33",
+    "url": "/kinobase/static/js/2.2af6bff2.chunk.js"
+  },
+  {
+    "revision": "dcf352602868d7eb0c18ba1eea441045",
+    "url": "/kinobase/static/js/2.2af6bff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf220c02fc2534234b3",
+    "url": "/kinobase/static/js/main.adbd3836.chunk.js"
+  },
+  {
+    "revision": "42e73ec6ff0bd78cf947",
+    "url": "/kinobase/static/js/runtime-main.a236f2ff.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/kinobase/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/kinobase/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/kinobase/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/kinobase/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/kinobase/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/kinobase/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/kinobase/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/kinobase/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/kinobase/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/kinobase/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/kinobase/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/kinobase/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/kinobase/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/kinobase/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/kinobase/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/kinobase/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/kinobase/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/kinobase/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/kinobase/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/kinobase/static/media/revicons.2feb69cc.eot"
+  }
+]);
